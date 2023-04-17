1b85603e050750d6904b13b42a57cdb0387a289b
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2023-04-17 18:42:41 -0500

    working on email form submission

--- src/scenes/Contact.jsx
+++ src/scenes/Contact.jsx
@@ -78,7 +78,7 @@ const Contact = () => {
           <form
             target="_blank"
             onSubmit={onSubmit}
-            action="https://formsubmit.co/e8a5bdfa807605332f809e5656e27c6e"
+            action=""
             method="POST"
           >
             <input
